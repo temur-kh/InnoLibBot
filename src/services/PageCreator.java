a4b0cf62658ee8d342b6b90c1025461ddde45b06
Rishat Maksudov <r.maksudov@innopolis.ru>
2018-02-03 18:00:57 +0300

    PageCreator class - Rishat. try to change account.

--- src/services/PageCreator.java
+++ src/services/PageCreator.java
@@ -1,9 +1,18 @@
 package services;
 
-import classes.Document.Document;
+import classes.Document.*;
+import org.telegram.telegraph.api.objects.*;
+
+import java.util.ArrayList;
 
 public class PageCreator {
-    public static String createDocumentPage(Document document) {
-        return null;
+    public static String createBookPage(Book book) {
+        ArrayList<Node> content = new ArrayList<>();
+        String title = book.getTitle();
+        String authorsNames = "";
+        for(String names : book.getAuthors()) {
+            authorsNames += names + "; ";
+        }
+        Node
     }
 }
